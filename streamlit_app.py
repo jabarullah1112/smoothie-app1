2fd0b73083a4e860fdc01ac1f53a622c783e95a2
jabarullah1112 <jabarullahtn@gmail.com>
2026-05-06 12:35:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,55 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+import pandas as pd
+import requests
+
+session = get_active_session()
+
+st.title("🍹 Smoothie Order App")
+
+# 🔹 Inputs
+name_on_order = st.text_input("Enter your name")
+
+fruit_df = session.table("smoothies.public.fruit_options").to_pandas()
+
+st.subheader("Available Fruits")
+st.dataframe(fruit_df)
+
+fruit_name_list = fruit_df["FRUIT_NAME"].tolist()
+fruit_map = dict(zip(fruit_df["FRUIT_NAME"], fruit_df["SEARCH_ON"]))
+
+ingredients_list = st.multiselect("Choose fruits", fruit_name_list)
+
+
+
+
+
+
+# Checkbox
+order_filled = st.checkbox("Order Filled")
+
+# Button
+submit_button = st.button("Submit Order")
+
+if submit_button:
+    if name_on_order and ingredients_string:
+
+        query = f"""
+        insert into smoothies.public.orders
+        (name_on_order, ingredients, order_filled)
+        values (
+            '{name_on_order}',
+            '{ingredients_string}',
+            {str(order_filled).upper()}
+        )
+        """
+
+        session.sql(query).collect()
+        st.success("Order placed successfully! ✅")
+
+        
+
+    else:
+        st.warning("Enter name and select fruits")
+
 
